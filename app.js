37534f1657d7c77c1f66b0ef0bc59edb7854b209
isabellav52 <123976477+isabellav52@users.noreply.github.com>
2023-02-08 10:14:37 -0500

    clase

---
+++ app.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded");
+const cardsAdj = [
+  {
+    name: `dulces1`,
+    img: `img/dulces1.webp`
+  },
+  {
+    name: `dulces2`,
+    img: `img/dulces2.jpg`
+  },
+  {
+    name: `dulces3`,
+    img: `img/dulces3.jpg`
+  },
+  {
+    name: `dulces4`,
+    img: `img/dulces3.jpg`
+  },
+  {
+    name: `dulces5`,
+    img: `img/dulces5.jpg`
+  },
+  {
+    name: `dulces6`,
+    img: `img/dulces6.jpg`
+  },
+  {
+    name: `dulces7`,
+    img: `img/dulces7.jpg`
+  },
+  {
+    name: `dulces8`,
+    img: `img/dulces8.jpg`
+  }
+];
